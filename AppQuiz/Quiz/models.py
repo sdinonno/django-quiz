b46f5bdfceb89b7be1e18469f692d097a4657433
Sofia Di Nonno <sofia@thelucky.app>
2023-06-19 16:55:19 -0300

    New models Question and ChooseAnswer

--- AppQuiz/Quiz/models.py
+++ AppQuiz/Quiz/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+class Question(models.Model):
+
+    text = models.TextField(verbose_name='Question text')
+
+    def __str__(self):
+        return self.text
+    
+
+class ChooseAnswer(models.Model):
+
+    question = models.ForeignKey(Question, related_name='questions', on_delete=models.CASCADE)
+    correct_asnwer = models.BooleanField(verbose_name='Is it the correct answer?', default=False, null=False)
+    text = models.TextField(verbose_name='Answer text')
+
+    def __str__(self) -> str:
+        return self.text
+
+
+
+
