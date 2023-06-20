d604ee6b35fe97a252435c2fbd40e1f0844f60ae
Sofia Di Nonno <sofia@thelucky.app>
2023-06-20 10:39:26 -0300

    New models in admin.py

--- AppQuiz/Quiz/admin.py
+++ AppQuiz/Quiz/admin.py
@@ -1,5 +1,16 @@
 from django.contrib import admin
 from .models import Question, ChooseAnswer
 
-admin.site.register(Question)
+class ChooseAnswerInLine(admin.TabularInline):
+    model = ChooseAnswer
+    max_num = ChooseAnswer.MAX_ANSWERS
+    min_num = ChooseAnswer.MAX_ANSWERS
+
+class QuestionAdmin(admin.ModelAdmin):
+    model = Question
+    inlines = (ChooseAnswerInLine, )
+    list_display = ['text',]
+    search_fields = ['text', 'questions__text']
+
+admin.site.register(Question, QuestionAdmin)
 admin.site.register(ChooseAnswer)
