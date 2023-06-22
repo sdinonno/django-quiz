5a1e65092621b970c8f0c830c49a943448f5ad66
Sofia Di Nonno <sofia@thelucky.app>
2023-06-22 11:45:19 -0300

    New models QuestionAttempts and User

--- AppQuiz/Quiz/models.py
+++ AppQuiz/Quiz/models.py
@@ -1,7 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Question(models.Model):
-
+    ALLOWED_ANSWERS = 1
     text = models.TextField(verbose_name='Question text')
 
     def __str__(self):
@@ -12,11 +13,23 @@ class ChooseAnswer(models.Model):
     MAX_ANSWERS = 3
 
     question = models.ForeignKey(Question, related_name='questions', on_delete=models.CASCADE)
-    correct_asnwer = models.BooleanField(verbose_name='Is it the correct answer?', default=False, null=False)
+    correct_answer = models.BooleanField(verbose_name='Is it the correct answer?', default=False, null=False)
     text = models.TextField(verbose_name='Answer text')
 
     def __str__(self) -> str:
         return self.text
+    
+class QuestionAttempts(models.Model):
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    answer = models.ForeignKey(ChooseAnswer, on_delete=models.CASCADE, related_name='attempts')
+    correct_answer = models.BooleanField(verbose_name='Is it the correct answer?', default=False, null=False)
+    score = models.DecimalField(verbose_name='Score', default=0, decimal_places=2, max_digits=6)
+
+class User(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    total_score = models.DecimalField(verbose_name='Total Score', default=0, decimal_places=2, max_digits=10)
+
 
 
 
