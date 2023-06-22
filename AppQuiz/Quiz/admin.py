7a2e85a7eaf6668d8edbbb42707fcd85117fbf18
Sofia Di Nonno <sofia@thelucky.app>
2023-06-22 11:37:18 -0300

    New class QuestionAttemptsAdmin and small changes

--- AppQuiz/Quiz/admin.py
+++ AppQuiz/Quiz/admin.py
@@ -1,10 +1,13 @@
 from django.contrib import admin
-from .models import Question, ChooseAnswer
+from .models import Question, ChooseAnswer, QuestionAttempts
+from .forms import ChooseInlineFormset
 
 class ChooseAnswerInLine(admin.TabularInline):
     model = ChooseAnswer
+    can_delete = False
     max_num = ChooseAnswer.MAX_ANSWERS
     min_num = ChooseAnswer.MAX_ANSWERS
+    formset = ChooseInlineFormset
 
 class QuestionAdmin(admin.ModelAdmin):
     model = Question
@@ -12,5 +15,12 @@ class QuestionAdmin(admin.ModelAdmin):
     list_display = ['text',]
     search_fields = ['text', 'questions__text']
 
+class QuestionAttemptsAdmin(admin.ModelAdmin):
+    list_display = ['question', 'answer', 'correct', 'score']
+
+    class Target:
+        model = QuestionAttempts
+
+admin.site.register(QuestionAttempts)
 admin.site.register(Question, QuestionAdmin)
 admin.site.register(ChooseAnswer)
